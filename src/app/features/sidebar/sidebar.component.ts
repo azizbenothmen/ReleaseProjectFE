a591682bf2fc49289ec107a4b2cb703440102c66
azizbenothmen <aziz.benothmen@enicar.ucar.tn>
2026-07-30 21:52:12 +0100

    adding sidebar

---
+++ src/app/features/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  constructor(private router: Router) {}
+
+  logout(): void {
+    this.router.navigate(['/login']);
+  }
+} 
